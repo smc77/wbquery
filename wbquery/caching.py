50371d7fabfcde6f59ab08a392efd02e0dc577c8
Skipper Seabold <jsseabold@gmail.com>
2011-08-04 19:52:36 -0400

    Note about caching

--- wbquery/caching.py
+++ wbquery/caching.py
@@ -10,6 +10,8 @@ from httplib2 import FileCache
 import gzip
 
 #TODO: Replace these with a proper db?
+#      caching could be more organized and aggressive, ie., caching actual
+#      indicators in a db and not the specific page views
 #NOTE: support for with statement in gzip only >= 2.7
 def save_cached_key(path, value):
     f = gzip.open(path, 'wb')
